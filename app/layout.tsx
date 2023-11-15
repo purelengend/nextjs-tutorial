7acbf35ded3403d6a7f9c0c55351a4e2fe2cc88f
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-11-15 17:18:11 +0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import { inter } from "./ui/fonts"
+
+import "@/app/ui/global.css"
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
